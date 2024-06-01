add66662f73b274ba31dbeae9f0018a896cb61c1
Tushar Bhowal <tusharbhowal3211@gmail.com>
2024-06-01 20:46:32 +0530

    navbar and message card done

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import { Button } from "./ui/button";
+import { User } from "next-auth";
+
+const Navbar = () => {
+  const { data: session } = useSession();
+  const user: User = session?.user;
+
+  return (
+    <nav className="p-4 shadow-sm text-black">
+      <div className="container mx-auto flex md:flex-row justify-between items-center">
+        <a href="#" className="text-xl font-bold transition-transform transform hover:scale-105">
+          ANONMESSAGE
+        </a>
+        {session ? (
+          <>
+            <span className="mr-4">Welcome, {user.username || user.email}</span>
+            <Button
+              onClick={() => signOut()}
+              className="w-full sm:px-8 hover:bg-black/85 hover:text-white rounded-lg md:w-auto bg-black text-white transition-transform transform hover:scale-105"
+              variant="outline"
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Link href="/sign-in">
+            <Button
+              className="w-full sm:px-8 hover:bg-black/85 hover:text-white rounded-lg md:w-auto bg-black text-white transition-transform transform hover:scale-105"
+              variant={"outline"}
+            >
+              Get Started
+            </Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
